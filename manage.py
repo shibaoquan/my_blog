e43d96e0890ddb5a047e9e5c467ee5468134a603
shibaoquan <1325022192@qq.com>
2018-09-14 17:54:56 +0800

    404优化，设置静态文件

--- manage.py
+++ manage.py
@@ -20,7 +20,7 @@ def user(name):
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('/404/spaceman.svg'), 404
 
 
 @app.errorhandler(500)
