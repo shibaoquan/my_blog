b1059b8899964cc4dbfe5569012b20179f54a31e
shibaoquan <1325022192@qq.com>
2018-09-14 23:09:38 +0800

    登录，session状态保持

--- manage.py
+++ manage.py
@@ -1,16 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_script import Manager
+from datetime import datetime
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
 
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+monment = Moment(app)
+
+# 跨站请求伪造保护
+app.config["SECRET_KEY"] = 'hard to guess string'
 
 
-@app.route('/')
-def index():
-    return "HELLO WORLD"
+# 创建表单类
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/user/<name>')
@@ -26,6 +38,15 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template("index.html", form=form, name=session.get("name"))
 
 
 if __name__ == '__main__':
