ead0fff0a0fcc2e6feb1061fd0ff9e29cc4ef111
shibaoquan <1325022192@qq.com>
2018-09-14 23:55:06 +0800

    连接数据库sqllite 定义模型类

--- manage.py
+++ manage.py
@@ -1,16 +1,24 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_script import Manager
 from datetime import datetime
 
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+import os
 
-
+# 数据库
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 monment = Moment(app)
@@ -23,6 +31,24 @@ app.config["SECRET_KEY"] = 'hard to guess string'
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
+
+
+class Role(db.Model):
+    __tablename__ = "roles"  # 表名
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, index=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 @app.route('/user/<name>')
@@ -44,6 +70,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get("name")
+        if old_name is not None and old_name != form.name.data:
+            flash("Looks like you have changed your name!")
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template("index.html", form=form, name=session.get("name"))
@@ -52,6 +81,3 @@ def index():
 if __name__ == '__main__':
     print(app.url_map)
     app.run(debug=True)
-
-
-
